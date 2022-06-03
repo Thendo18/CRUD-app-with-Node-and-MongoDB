257c4668377b3f80f2af41e0a7ecd5ffc1818451
Thendo18 <thendomakherana@gmail.com>
2022-06-04 00:07:09 +0200

    changes

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
-const bodyParser= require('body-parser')
-const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+const bodyParser= require('body-parser');
+
+// console.log('May Node be with you')
+
+//This part shows when making callbacks
+// MongoClient.connect("connectionString", {
+//   useUnifiedTopology: true
+// }, (err, client) => {
+//   if (err) return console.error(err)
+//   console.log('Connected to Database')
+// })
+
+
+// using promises
+MongoClient.connect("mongodb+srv://Thendo:<Thendo@12345>@cluster0.8x1wf.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('crud-app-quotes')
+    const quotesCollection = db.collection('quotes')
+ 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
@@ -17,6 +37,13 @@ app.get('/', (req, res) => {
 
 
   app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
+    quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
   })
-// console.log('May Node be with you')
+
+  })
+  .catch(console.error)
