ded36ffcd15c6a2cf1c5b13ead70b5e04c7eca2d
Thendo18 <thendomakherana@gmail.com>
2022-06-03 12:59:50 +0200

    server and browser communicating

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -10,5 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  })
+
+
+  app.post('/quotes', (req, res) => {
+    console.log(req.body)
   })
 // console.log('May Node be with you')
