f1f8092e815eb071483c38f1333d51ad1aa90b31
Thendo18 <thendomakherana@gmail.com>
2022-06-04 19:25:24 +0200

    changes

--- config/ database.config.js
+++ config/ database.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
-    url: 'mongodb+srv://Thendo:<Thendo12345>@cluster0.8x1wf.mongodb.net/?retryWrites=true&w=majority'
+    url: 'mongodb+srv://Thendo:Thendo12345@cluster0.8x1wf.mongodb.net/?retryWrites=true&w=majority'
 }
