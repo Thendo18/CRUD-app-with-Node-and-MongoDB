71eb30cda4f4b4ae15d18bd0db78657eff20b371
Thendo18 <thendomakherana@gmail.com>
2022-06-04 20:37:04 +0200

    done

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const UserRoute = require('./app/routes/User')
+const UserRoute = require('./app/routes/user')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
-app.use('/user',UserRoute)
 
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
@@ -18,15 +17,18 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Databse Connected Successfully!!");    
+    console.log("Database Connected Successfully!!");    
 }).catch(err => {
     console.log('Could not connect to the database', err);
     process.exit();
 });
 
 
+
+app.use('/user',UserRoute)
+
 app.get('/', (req, res) => {
-    res.json({"message": "Hello Crud Node Express"});
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.listen(3000, () => {
