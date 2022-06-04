128ff95953c3ad91f4edd67e1ddc7a08a670a3b6
Thendo18 <thendomakherana@gmail.com>
2022-06-04 18:22:35 +0200

    changes

---
+++ config/ database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb+srv://Thendo:<Thendo12345>@cluster0.8x1wf.mongodb.net/?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,49 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const MongoClient = require('mongodb').MongoClient;
 const app = express();
-const bodyParser= require('body-parser');
+const UserRoute = require('./app/routes/User')
 
-// console.log('May Node be with you')
-
-//This part shows when making callbacks
-// MongoClient.connect("connectionString", {
-//   useUnifiedTopology: true
-// }, (err, client) => {
-//   if (err) return console.error(err)
-//   console.log('Connected to Database')
-// })
-
-
-// using promises
-MongoClient.connect("mongodb+srv://Thendo:<Thendo@12345>@cluster0.8x1wf.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('crud-app-quotes')
-    const quotesCollection = db.collection('quotes')
- 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+app.use(bodyParser.json())
 
-// app.get(endpoint, callback)
+app.use('/user',UserRoute)
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");    
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  })
+    res.json({"message": "Hello Crud Node Express"});
+});
 
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
 
-  app.post('/quotes', (req, res) => {
-    // console.log(req.body)
-    quotesCollection.insertOne(req.body)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.error(error))
-  })
-
-  })
-  .catch(console.error)
